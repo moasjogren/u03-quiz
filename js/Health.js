b405ab41c05e0ba508c3475eb3fdeac22c239a82
Simon Kane <Simon.kaneborn@chasacademy.se>
2024-11-14 11:37:05 +0100

    Fixilurat med kattlogon

--- js/Health.js
+++ js/Health.js
@@ -28,14 +28,14 @@ class Health {
 
     for (let i = 0; i < this.health; i++) {
       let heart = document.createElement("img");
-      heart.src = "./img/image 5.svg";
+      heart.src = "./img/image5.png";
       heart.alt = "Heart";
       this.parentElement.appendChild(heart);
     }
 
     for (let i = 0; i < 3 - this.health; i++) {
       let heart = document.createElement("img");
-      heart.src = "./img/image 6.svg";
+      heart.src = "./img/image6.png";
       heart.alt = "Broken heart";
       this.parentElement.appendChild(heart);
     }
