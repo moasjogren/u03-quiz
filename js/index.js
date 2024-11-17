3bf65eb38fb76cf7c75013aa689167b2c5ec42f6
SixtenE <sixten.ekblad@gmail.com>
2024-11-17 14:11:58 +0100

    testade en grej

--- js/index.js
+++ js/index.js
@@ -1,223 +1,256 @@
-const generalKnowledge = "https://opentdb.com/api.php?amount=1&category=9&type=multiple";
-const geography = "https://opentdb.com/api.php?amount=1&category=22&type=multiple";
-const art = "https://opentdb.com/api.php?amount=1&category=25&type=multiple";
-const sports = "https://opentdb.com/api.php?amount=1&category=21&type=multiple";
-const music = "https://opentdb.com/api.php?amount=1&category=12&type=multiple";
-const history = "https://opentdb.com/api.php?amount=1&category=23&type=multiple";
-const animals = "https://opentdb.com/api.php?amount=1&category=27&type=multiple";
-const science = "https://opentdb.com/api.php?amount=1&category=17&type=multiple";
-const lifeElement = document.querySelector(".quiz-container__life");
+const amount = 1
+const type = 'multiple'
 
-const questionBox = document.querySelector(".quiz-container__gameboard__questions");
-const currentScore = document.querySelector(".quiz-container__gameboard__points__score");
-const highScore = document.querySelector(".highscore");
+const categories = [
+  {
+    id: 9,
+    name: 'General Knowledge',
+    color: '#ef4444',
+    borderColor: '#dc2626',
+    icon: '💡',
+  },
+  {
+    id: 22,
+    name: 'Geography',
+    color: '#f97316',
+    borderColor: '#ea580c',
+    icon: '🌎',
+  },
+  {
+    id: 25,
+    name: 'Art',
+    color: '#eab308',
+    borderColor: '#ca8a04',
+    icon: '🎨',
+  },
+  {
+    id: 21,
+    name: 'Sports',
+    color: '#84cc16',
+    borderColor: '#65a30d',
+    icon: '⚽️',
+  },
+  {
+    id: 12,
+    name: 'Music',
+    color: '#06b6d4',
+    borderColor: '#0891b2',
+    icon: '🎵',
+  },
+  {
+    id: 23,
+    name: 'History',
+    color: '#3b82f6',
+    borderColor: '#2563eb',
+    icon: '📜',
+  },
+  {
+    id: 27,
+    name: 'Animals',
+    color: '#8b5cf6',
+    borderColor: '#7c3aed',
+    icon: '🐱',
+  },
+  {
+    id: 17,
+    name: 'Science',
+    color: '#ec4899',
+    borderColor: '#db2777',
+    icon: '🔬',
+  },
+]
 
-let score = 0;
+let url = null
+let selectedCategory = null
+let questions = []
+let currentQuestion = null
+let selectedAnswer = null
+let questionIndex = 0
+let timer = null
+let score = 0
 
-if (localStorage.getItem("highscore")) highScore.innerHTML = localStorage.getItem("highscore");
+const categoryContainer = document.querySelector('.category-container')
+const questionContainer = document.querySelector('.question-container')
+const questionTitle = document.querySelector('.question-container h1')
+const answerContainer = document.querySelector('.answer-container')
+const quizFooter = document.querySelector('.quiz-footer')
+const quizTitle = document.querySelector('.quiz-header h1')
 
-const displayCategory = document.querySelector(".quiz-container__gameboard__categories-h2");
+let highScore = 0
 
-// Array med kategorierna
-const categories = ["generalKnowledge", "geography", "art", "sports", "music", "history", "animals", "science"];
-
-// Funktion för slumpmässigt val av 4 kategorier
-function getRandomOrder(arr) {
-  let randomArray = arr.sort(() => Math.random() - 0.5).slice(0, 4);
-  return randomArray;
+function getHighScore() {
+  highScore = parseInt(localStorage.getItem('highScore')) || 0
+  quizFooter.querySelector(
+    '.high-score',
+  ).textContent = `High Score: ${highScore}`
 }
 
-function changeCatImg(img) {
-  const catImg = document.querySelector(".cat-image");
-  catImg.src = `./img/cat${img}.png`;
-  catImg.style.scale = "0.3";
+resetQuiz()
+getHighScore()
+
+answerContainer
+  .querySelectorAll('.answer-container__item')
+  .forEach((item, index) => {
+    item.addEventListener('click', () => {
+      selectAnswer(index)
+    })
+  })
+
+function addCategoryElement(category) {
+  const item = document.createElement('div')
+
+  item.classList.add('category-container__item')
+  item.style.backgroundColor = `${category.color}`
+  item.style.borderBottom = `0.5rem solid ${category.borderColor}`
+
+  const iconSpan = document.createElement('span')
+  iconSpan.textContent = category.icon
+
+  item.appendChild(iconSpan)
+
+  item.addEventListener('click', () => {
+    selectCategory(category.id)
+  })
+
+  categoryContainer.appendChild(item)
+}
+
+function showCategories() {
+  ;[...categories]
+    .sort(() => Math.random() - 0.5)
+    .splice(0, 4)
+    .forEach((category) => {
+      addCategoryElement(category)
+    })
+}
+
+function selectCategory(category) {
+  selectedCategory = category
+
+  quizTitle.textContent = categories.find((c) => c.id === category).name
+
+  categoryContainer.classList.add('hidden')
+  questionContainer.classList.remove('hidden')
+
   setTimeout(() => {
-    catImg.style.scale = "1";
-  }, 250);
+    getQuestions()
+  }, 1000)
 }
 
-function createCats() {
-  let randomCats = getRandomOrder(categories);
-  randomCats.forEach((cat) => {
-    let genDivs = document.createElement("div");
-    let genImg = document.createElement("img");
-    genImg.src = `./img/${cat}.png`;
-    document.querySelector(".quiz-container__gameboard__categories").append(genDivs);
-    genDivs.append(genImg);
-    genDivs.classList.add(cat);
-    genDivs.classList.add("bubble");
-    genImg.classList.add("bubble__img");
-    genImg.addEventListener("click", function () {
-      switch (cat) {
-        case "generalKnowledge":
-          getQuestion(generalKnowledge);
-          displayCategory.innerHTML = "General knowledge";
-          break;
-        case "geography":
-          getQuestion(geography);
-          displayCategory.innerHTML = "Geography";
-          break;
-        case "art":
-          getQuestion(art);
-          displayCategory.innerHTML = "Art";
-          break;
-        case "sports":
-          getQuestion(sports);
-          displayCategory.innerHTML = "Sports";
-          break;
-        case "music":
-          getQuestion(music);
-          displayCategory.innerHTML = "Music";
-          break;
-        case "history":
-          getQuestion(history);
-          displayCategory.innerHTML = "History";
-          break;
-        case "animals":
-          getQuestion(animals);
-          displayCategory.innerHTML = "Animals";
-          break;
-        case "science":
-          getQuestion(science);
-          displayCategory.innerHTML = "Science";
-          break;
-      }
-      document.querySelector(".card").classList.toggle("flipped");
-      changeCatImg(1);
-    });
-  });
-}
-async function getQuestion(cat) {
+async function getQuestions() {
+  if (!selectCategory) return
   try {
-    const response = await fetch(cat);
+    const response = await fetch(
+      `https://opentdb.com/api.php?amount=${amount}&type=${type}&category=${selectedCategory}`,
+    )
     if (!response.ok) {
-      throw new Error("HTTP" + response.status);
+      throw new Error('HTTP' + response.status)
     }
-    const data = await response.json();
-    genQuestion(data.results[0]);
+    const data = await response.json()
+
+    questions = data.results.map((q) => {
+      return {
+        question: removeHTMLEntities(q.question),
+        correctAnswer: removeHTMLEntities(q.correct_answer),
+        options: [...q.incorrect_answers, q.correct_answer]
+          .sort(() => Math.random() - 0.5)
+          .map((option) => removeHTMLEntities(option)),
+      }
+    })
+
+    currentQuestion = questions[0]
+    showQuestion(currentQuestion)
   } catch (error) {
-    console.log(error);
+    console.log(error)
   }
 }
 
-function genQuestion(data) {
-  const question = data.question;
-  const questionTitle = document.createElement("h2");
-  questionTitle.classList.add("question");
-  questionTitle.innerHTML = question;
-  questionBox.append(questionTitle);
-  const rightAnswer = data.correct_answer;
-  const options = [...data.incorrect_answers, rightAnswer];
-  const randomOrder = getRandomOrder(options);
-  randomOrder.forEach((text) => {
-    const option = document.createElement("div");
-    option.classList.add("answer");
-    option.innerHTML = text;
-    questionBox.append(option);
-    option.addEventListener("click", (e) => {
-      checkAnswer(text, rightAnswer);
-      showCorrectAnswer(document.querySelectorAll(".answer"), rightAnswer);
-      console.log(e.target);
-    });
-  });
+function showQuestion(q) {
+  questionTitle.textContent = currentQuestion.question
+
+  setTimeout(() => {
+    showOptions()
+  }, 1000)
 }
 
-function checkAnswer(correct, guess) {
-  if (guess === correct) {
-    addTime();
-    updateScore();
-    console.log("CORRECT");
+function showOptions() {
+  answerContainer.classList.remove('loading')
+
+  const items = answerContainer.querySelectorAll('.answer-container__item')
+
+  items.forEach((item, index) => {
+    item.textContent = currentQuestion.options[index]
+  })
+}
+
+function selectAnswer(index) {
+  console.log(index)
+
+  const isCorrect =
+    currentQuestion.options[index] === currentQuestion.correctAnswer
+
+  if (isCorrect) {
+    console.log('correct')
   } else {
-    console.log("WRONG");
+    console.log('wrong')
   }
+
+  answerContainer
+    .querySelectorAll('.answer-container__item')
+    .forEach((item, index) => {
+      if (currentQuestion.options[index] === currentQuestion.correctAnswer) {
+        item.classList.add('correct')
+      } else {
+        item.classList.add('wrong')
+      }
+    })
+
+  score += isCorrect ? 1 : 0
+
+  setTimeout(() => {
+    updateScore()
+  }, 1000)
 }
-
-function showCorrectAnswer(array, correct) {
-  array.forEach((text) => {
-    if (text.innerHTML === correct) {
-      text.style.backgroundColor = "green";
-    } else {
-      text.style.backgroundColor = "red";
-    }
-  });
-}
-
-createCats();
-
-// Game over-state nedan
-const gaga = new Health(lifeElement, () => {
-  console.log("game over");
-});
 
 function updateScore() {
-  score++;
-
-  currentScore.innerHTML = `Score: ${score}`;
-  if (score > localStorage.getItem("highscore")) {
-    localStorage.setItem("highscore", score);
+  if (score > highScore) {
+    highScore = score
+    localStorage.setItem('highScore', highScore)
   }
-}
-window.gaga = gaga;
 
-// quiz-container__gameboard__categories
-// Nummer för  alla kategorier:
-// generalKnowledge = 9
-// geography = 22
-// art = 25
-// sports = 21
-// music = 12
-// history 23
-// animals = 27
-// science = 17
+  quizFooter.querySelector('.score').textContent = `Score: ${score}`
+  quizFooter.querySelector(
+    '.high-score',
+  ).textContent = `High Score: ${highScore}`
 
-/*
-const categories = {
-  generalknowledge: 9,
-  geography: 22,
-  art: 25,
-  sports: 21,
-  music: 12,
-  history: 23,
-  animals: 27,
-  science: 17,
-}
-*/
-
-//globala variablar
-const quizDuration = 10;
-let secondsLeft;
-let countdownInterval;
-const timeAddedByRightAnswer = 3;
-let timerInProgress = false;
-
-function startCountdown() {
-  if (timerInProgress) return;
-  timerInProgress = true;
-  secondsLeft = quizDuration;
-  document.querySelector("#timer").style.width = "100%";
-  document.getElementById("timer").style.backgroundColor = "#0ea5e9";
-
-  countdownInterval = setInterval(() => {
-    if (secondsLeft > 0) {
-      if (secondsLeft === 6) {
-        document.getElementById("timer").style.backgroundColor = "#ff0000";
-      } else if (secondsLeft > 6) {
-        document.getElementById("timer").style.backgroundColor = "#0ea5e9";
-      }
-      secondsLeft -= 1;
-      document.getElementById("start-btn").textContent = secondsLeft;
-      document.getElementById("timer").style.width = `${(secondsLeft / quizDuration) * 100}%`;
-    } else {
-      alert("Time's up! You can allways take the quiz again!");
-      timerInProgress = false;
-      clearInterval(countdownInterval);
-    }
-  }, 1000);
+  setTimeout(() => {
+    resetQuiz()
+  }, 1000)
 }
 
-function addTime() {
-  secondsLeft += timeAddedByRightAnswer;
-  if (secondsLeft > quizDuration) {
-    secondsLeft = quizDuration;
-  }
+function resetQuiz() {
+  selectedAnswer = null
+  selectedCategory = null
+
+  questionTitle.textContent = '...'
+  answerContainer.classList.add('loading')
+  answerContainer
+    .querySelectorAll('.answer-container__item')
+    .forEach((item) => {
+      item.textContent = ''
+      item.classList.remove('correct')
+      item.classList.remove('wrong')
+    })
+
+  categoryContainer.classList.remove('hidden')
+  questionContainer.classList.add('hidden')
 }
+
+function removeHTMLEntities(html) {
+  const doc = new DOMParser().parseFromString(html, 'text/html')
+  return doc.documentElement.textContent
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  showCategories()
+})
